a6c0fe75a8c42071d75fc900c4baf509ef400b79
Ármann Hallbert Jónsson <armann.hj@outlook.com>
2018-05-11 18:27:58 +0000

    Sitja upp https - tölvubraut

---
+++ https - tölvubraut.ps1
@@ -0,0 +1,19 @@
+﻿$notendur = Import-Csv C:\Users\Administrator\Documents\Powershell\file.csv
+
+foreach($n in $notendur){
+    
+
+
+        $Sam = $n.samaccountname
+
+        $Sam
+        
+
+        
+        New-Item $("C:\inetpub\wwwroot\" + $Sam + ".eep.is") -ItemType Directory
+        New-Item $("C:\inetpub\wwwroot\" + $Sam + ".eep.is\index.html") -ItemType File -Value $("Notendasiða " + $Sam + "")
+        New-Website -Name $($Sam + ".eep.is") -HostHeader $($Sam + ".eep.is") -PhysicalPath $("C:\inetpub\wwwroot\" + $Sam + ".eep.is\")
+
+
+
+}
