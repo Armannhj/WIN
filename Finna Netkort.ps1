37cc0c64d6c7a2694803401819fd77179ef2da39
Ármann Hallbert Jónsson <armann.hj@outlook.com>
2018-05-11 18:27:33 +0000

    Netkort script

---
+++ Finna Netkort.ps1
@@ -0,0 +1,16 @@
+﻿### FINNA NETKORT ####
+
+function FinnaNafnNetkortsMedIpTolu{
+    param(
+        [Parameter(Mandatory=$true, HelpMessage="Sl��u inn ip t�lvu e�a hluta af henni. Settu * ef ekki �ll Ip-talan")]
+        [string]$TakaInnIpTol
+        )
+
+        $tala = $TakaInnIpTol
+        $ip = (Get-NetIPAddress -IPAddress $tala).InterfaceAlias
+        if($ip){
+            $ip
+        }else{
+        Write-Error -Message "FinnanafnNetkorstMedIPTolu : Fann ekki netkort me� ip t�luna " + $tala
+        }
+}
