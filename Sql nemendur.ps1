e216d4c7e9d2457e3e0f8de88d2b7117a81e62e1
Ármann Hallbert Jónsson <armann.hj@outlook.com>
2018-05-12 12:54:22 +0000

    Add files via upload

---
+++ Sql nemendur.ps1
@@ -0,0 +1,22 @@
+﻿$notendur = Import-Csv C:\Users\Administrator\Documents\Powershell\file.csv
+
+foreach($n in $notendur){
+    
+
+
+        $Sam = $n.samaccountname
+
+        $Sam
+        
+
+        
+        Invoke-Sqlcmd -ServerInstance '(local)' -Query "use master 
+        go
+        create database $Sam
+        go
+        CREATE LOGIN [EEP-ARMANN\$Sam] FROM WINDOWS WITH DEFAULT_DATABASE=[$Sam]
+        go
+        use $Sam"
+
+
+}
